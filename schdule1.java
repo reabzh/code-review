2846a56d72a03921e7f84b59778c9de73d35e758
reabzh <114014846+reabzh@users.noreply.github.com>
2022-09-21 15:05:15 +0300

    Update schdule1.java

--- schdule1.java
+++ schdule1.java
@@ -1,21 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Assignment;
 
-/**
- *
- * @author reema
- */
 public class schdule {
+    
+    
+    schdule(){
+        System.out.println";
+        }
     
     schdule schdules; //we assume that all schdule alredy in the system
     int days[]= new int[31];
     int months[]= new int[12];
     int years[]= new int[2022];
     
+    String deletSchdule;
+    int NumSchdule;
     
     //we assume that all schdule alredy in the system
     public void searchInSchdule( int day, int month, int year){
